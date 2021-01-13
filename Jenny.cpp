37380a266df0b9da99a68415a8c4fa632729d25a
qwe15236 <67190635+qwe15236@users.noreply.github.com>
2021-01-13 21:16:13 +0800

    Create Jenny.cpp

---
+++ Jenny.cpp
@@ -0,0 +1,22 @@
+// User1.0.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include <conio.h>
+#include "server.h"
+
+using namespace std;
+
+int main()
+{
+    string ID = "JennyID";
+    cout << "Jenny 1.0 请发送信息(exit 退出 | clear 清除信息)\n" << endl;
+    string str;
+    while (1)
+    {
+        cout << "Jenny: ";
+        cin >> str;
+        Information(str, ID);
+        accept(ID);
+    }
+}
